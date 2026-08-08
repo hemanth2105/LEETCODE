a64c7a0f078943172a7034a7cab779f226f62591
Saihemanth Chandra <saihemanthchandra9@gmail.com>
2026-08-08 08:23:35 +0530

    Time: 1 ms (70.60%) | Memory: 42.6 MB (75.85%) - LeetSync

---
+++ 504-base-7/base-7.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String convertToBase7(int num) {
+        if (num == 0) {
+            return "0";
+        }
+
+        boolean negative = num < 0;
+        num = Math.abs(num);
+
+        StringBuilder sb = new StringBuilder();
+
+        while (num > 0) {
+            int rem = num % 7;
+            sb.append(rem);
+            num = num / 7;
+        }
+
+        if (negative) {
+            sb.append("-");
+        }
+
+        return sb.reverse().toString();
+    }
+}
